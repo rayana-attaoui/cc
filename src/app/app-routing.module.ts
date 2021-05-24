a40b7006e469f2249daeb2eadcf33eb1f8dda675
rayana attaoui <attaouirayana123@gmail.com>
2021-05-24 18:39:09 +0200

    first

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component:AppComponent,
+  },
+  {
+    path:'home',
+    component:HomeComponent,
+  },
+  {
+    path:'navbar',
+    component:NavbarComponent,
+  },
+  {
+    path:'footer',
+    component:FooterComponent,
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
